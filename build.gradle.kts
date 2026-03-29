4bb7a39cf429f5bee55a061b07965c608f0558d3
Simon Kurz <simonlukas.98@gmail.com>
2026-03-29 22:32:15 +0200

    Use more recent API version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("net.portswigger.burp.extensions:montoya-api:2025.11")
+    compileOnly("net.portswigger.burp.extensions:montoya-api:2026.2")
 }
 
 tasks.withType<JavaCompile>().configureEach {

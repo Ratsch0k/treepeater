eca908c5997831e9dba66c5c91d2e8fc3734464e
Simon Kurz <simonlukas.98@gmail.com>
2026-05-03 12:19:28 +0200

    Add new icons for sync and create folder buttons

---
+++ src/main/java/treepeater/icons/CreateNewFolderIcon.java
@@ -0,0 +1,9 @@
+package treepeater.icons;
+
+import treepeater.components.SvgIcon;
+
+public class CreateNewFolderIcon extends SvgIcon {
+    public CreateNewFolderIcon() {
+        super("/icons/create_new_folder.svg");
+    }
+}
